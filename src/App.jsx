6aad9791d97092e1971b4b1aea70b5bb1e182abc
24dimitris <24dimitris@gmail.com>
2025-02-04 19:26:30 +0000

    Created all basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
 import QRCode from 'react-qr-code'
+import Button from './components/Button'
+import * as htmlToImage from 'html-to-image';
+import * as FileSaver from 'file-saver';
 
 function App() {
-  const [link, setLink] = useState("www.google.com")
+  const [link, setLink] = useState("www.google.com");
+
+  const printPng = () => {
+  htmlToImage.toBlob(document.getElementById('QRCode'))
+    .then(function (blob) {
+      if (window.saveAs) {
+        window.saveAs(blob, 'qr-code.png');
+      } else {
+       FileSaver.saveAs(blob, 'qr-code.png');
+     }
+    });
+  }
 
   return (
     <div className='Container'>
-      <QRCode value={link} />
-      <input type='text' value={link} onChange={(e)=>{setLink(e.target.value)}}/>
+      <h1 className='Title'>Simple QR Code Generator</h1>
+      <QRCode size={512} className='QRCode' id="QRCode" value={link} />
+      <input className='Input' maxLength={300} type='text' value={link} onChange={(e)=>{setLink(e.target.value)}}/>
+      <div className='Downloads' onClick={printPng}>
+      <Button>Save PNG</Button>
+      </div>
     </div>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import styles from './Button.module.css'
+
+export default function Button(props){
+
+    return(
+        <button className={styles.SaveButton}>{props.children}</button>
+    )
+
+}
