1656eac02041ce666f0aee435c2be2b054d7e5ac
24dimitris <24dimitris@gmail.com>
2025-02-05 10:14:57 +0000

    Added Color Selection basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import QRCode from 'react-qr-code'
 import Button from './components/Button'
 import * as htmlToImage from 'html-to-image';
 import * as FileSaver from 'file-saver';
+import { SketchPicker } from 'react-color';
 
 function App() {
   const [link, setLink] = useState("www.google.com");
+  const [color,setColor] = useState("#000000");
+  const [bgColor,setBgColor] = useState("#ffffff");
 
   const printPng = () => {
   htmlToImage.toBlob(document.getElementById('QRCode'))
@@ -18,15 +21,41 @@ function App() {
      }
     });
   }
+  
 
   return (
     <div className='Container'>
+
       <h1 className='Title'>Simple QR Code Generator</h1>
-      <QRCode size={512} className='QRCode' id="QRCode" value={link} />
+      <QRCode style={{backgroundColor: bgColor}} fgColor={color} bgColor={bgColor} size={400} className='QRCode' id="QRCode" value={link} />
       <input className='Input' maxLength={300} type='text' value={link} onChange={(e)=>{setLink(e.target.value)}}/>
+      
+      <div className='ColorsContainer'>
+        <div className='ColorPicker'>
+          <span>Foreground Color</span>
+          <button className='ColorButton' style={{backgroundColor:color}} onClick={()=>{console.log("among")}}/>
+          <SketchPicker
+          color={color}
+          onChange={(color)=>{setColor(color.hex)}}
+          disableAlpha={true}
+          />
+        </div>
+    
+        <div className='ColorPicker'>
+          <span>Background Color</span>
+          <button className='ColorButton' style={{backgroundColor:bgColor}} onClick={()=>{console.log("among")}}/>
+          <SketchPicker
+          color={bgColor}
+          onChange={(bgColor)=>{setBgColor(bgColor.hex)}}
+          disableAlpha={true}
+          />
+        </div>
+      </div>
+
       <div className='Downloads' onClick={printPng}>
       <Button>Save PNG</Button>
       </div>
+      
     </div>
   )
 }
